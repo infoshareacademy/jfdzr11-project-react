b677f58f92b83b5317b60bf58bef1d3268c05ddf
Bartłomiej Galicki <bartek.galicki@gmail.com>
2023-05-14 11:14:29 +0200

    bulk upload project files

---
+++ 04-typescript/src/components/Definition/Definition.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { Definition } from "../../types/SearchedWordData";
+
+const Definition: React.FC<Definition> = () => {
+  return <div>Definition</div>;
+};
+
+export default Definition;
